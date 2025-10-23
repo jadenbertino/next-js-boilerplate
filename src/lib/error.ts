c70664934452af810308b306771711c388aff4b7
Jaden Bertino <jaden@bertinofamily.com>
2025-10-23 16:22:16 -0700

    update error classes

--- src/lib/error.ts
+++ src/lib/error.ts
@@ -2,23 +2,31 @@ type HttpErrorArgs = {
   message: string
   details?: unknown
   statusCode: number
+  [key: string]: unknown
 }
 
 class HttpError extends Error {
   details?: unknown
-  statusCode: number
+  statusCode: number;
+  [key: string]: unknown
 
-  constructor({ message, details, statusCode }: HttpErrorArgs) {
+  constructor({
+    message,
+    details,
+    statusCode,
+    ...extraProperties
+  }: HttpErrorArgs) {
     super(message)
     this.name = this.constructor.name // Ensure the name of this error is set as 'HttpError'
     this.details = details
     this.statusCode = statusCode
+    Object.assign(this, extraProperties)
   }
 }
 
 class BadRequestError extends HttpError {
-  constructor({ message, statusCode, details }: HttpErrorArgs) {
-    super({ message, statusCode, details })
+  constructor(error: HttpErrorArgs) {
+    super(error)
   }
 }
 
